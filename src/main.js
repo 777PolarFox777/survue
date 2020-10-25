4e9ad1f1bd9dab507e920c506643c677459ae1a0
andrei_dmitrov <adimitrov@ebsco.com>
2020-10-25 14:19:04 +0300

    Use Vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueCompositionApi from '@vue/composition-api';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import store from './store'
 
 Vue.use(VueCompositionApi);
 
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
